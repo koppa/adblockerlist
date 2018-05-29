a3e9d9ff40827def50fcdfd2dc221adfebbc4c8b
Markus Gräb <m_graeb11@cs.uni-kl.de>
2018-05-29 11:52:36 +0200

    Created docs  folder

--- download_and_join.sh
+++ download_and_join.sh
@@ -8,4 +8,5 @@ wget -N `cat ../lists`
 cd ..
 cat down/* > joined
 cat joined | dos2unix | grep -v "^#" |rev | cut -d" " -f 1 | cut -d$'\t' -f 1 | rev | sort | grep -ve '^$' | uniq > joined_clean
+mv joined_clean docs/
 
